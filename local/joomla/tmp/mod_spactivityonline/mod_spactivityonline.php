7885e73518cc34335ae171b3cfd25342daf28bd4
adolfozignago <adolfo.zignago@esmartit.es>
2020-05-23 20:16:45 +0200

    add new module files

---
+++ local/joomla/tmp/mod_spactivityonline/mod_spactivityonline.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     SmartPoke.Site
+ * @subpackage  mod_spactivityonline
+ *
+ * @copyright   Copyright (C) 2020 eSmartIT. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+require JModuleHelper::getLayoutPath('mod_spactivityonline', $params->get('layout', 'default'));
