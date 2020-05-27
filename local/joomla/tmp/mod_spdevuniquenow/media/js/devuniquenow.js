179789a971661eab3cfe9a51dec27b9ec597e69a
adolfozignago <adolfo.zignago@esmartit.es>
2020-05-27 22:35:43 +0200

    endpoint modified in devuniquenow.js

--- local/joomla/tmp/mod_spdevuniquenow/media/js/devuniquenow.js
+++ local/joomla/tmp/mod_spdevuniquenow/media/js/devuniquenow.js
@@ -1,16 +1,10 @@
 $(document).ready( function() {
-    const sourceEvt = new EventSource("index.php?option=com_spserverevent&format=json&resource_path=/sensor-activity/hourly-device-presence-delta");
-    let inCount = 0;
-    let limitCount = 0;
-    let outCount = 0;
+    const sourceEvt = new EventSource("index.php?option=com_spserverevent&format=json&resource_path=/sensor-activity/minute-device-total-count");
     let deviceNow = 0;
 
     sourceEvt.onmessage = function (event) {
         let eventData = JSON.parse(event.data);
-        inCount = eventData.inCount;
-        limitCount = eventData.limitCount;
-        outCount = eventData.outCount;
-        deviceNow = inCount + limitCount + outCount;
+        deviceNow = eventData.count;
         document.getElementById("devuniquenow").innerHTML = Intl.NumberFormat().format(deviceNow);
     }
 });
