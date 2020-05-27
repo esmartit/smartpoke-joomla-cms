5d6d95ee8365f153ce5e48aafcd3c0038525e980
adolfozignago <adolfo.zignago@esmartit.es>
2020-05-27 17:30:25 +0200

    hide timezone

--- local/joomla/tmp/mod_spactivityonline/tmpl/default.php
+++ local/joomla/tmp/mod_spactivityonline/tmpl/default.php
@@ -23,7 +23,7 @@ $document->addScript('/media/mod_spactivityonline/js/echartactivityonline.js');
             <div class="clearfix"></div>
         </div>
         <div class="x_content">
-            <div id="userTimeZone"><b><?php echo $usertimezone; ?></b></div>
+            <div id="userTimeZone" style="display:none"><b><?php echo $usertimezone; ?></b></div>
             <div id="echart_activity_online" class="demo-placeholder" style="height:350px;"></div>
         </div>
     </div>
