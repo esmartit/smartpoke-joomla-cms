100421c86868c5dbcdb86352db36c03457e7ab52
adolfozignago <adolfo.zignago@esmartit.es>
2020-07-17 09:41:21 +0200

    Update el modulo dailyGoalRegistered. Fix (show total registered today)

--- local/joomla/tmp/mod_spdailygoal/media/js/gaugedailygoalreg.js
+++ local/joomla/tmp/mod_spdailygoal/media/js/gaugedailygoalreg.js
@@ -29,12 +29,12 @@ $(document).ready( function() {
         var chart_gauge_reg = document.getElementById('chart_gauge_dailygoal_reg'); // your canvas element
         var chart_gauge_dailygoal_reg = new Gauge(chart_gauge_reg).setOptions(option); // create sexy gauge!
     }
-    dailygoal_reg = 0;
+
     if ($('#gauge-text-reg').length) {
         chart_gauge_dailygoal_reg.setTextField(document.getElementById("gauge-text-reg"));
         chart_gauge_dailygoal_reg.animationSpeed = 32; // set animation speed (32 is default value)
         chart_gauge_dailygoal_reg.maxValue = dailyGoalRegMaxValue;
-        chart_gauge_dailygoal_reg.set(dailygoal_reg);
+        chart_gauge_dailygoal_reg.set(dailyGoalReg);
     }
 
     seDailyGoalReg.onmessage = function (event) {
@@ -43,8 +43,8 @@ $(document).ready( function() {
         let today = new Date(eventData.time);
         let sameDate = (currentDate.getDate() === today.getUTCDate());
 
-        if ($('#goal-text-reg').length) {
-            chart_gauge_dailygoal_reg.setTextField(document.getElementById("goal-text-reg"));
+        if ($('#gauge-text-reg').length) {
+            chart_gauge_dailygoal_reg.setTextField(document.getElementById("gauge-text-reg"));
             chart_gauge_dailygoal_reg.animationSpeed = 32; // set animation speed (32 is default value)
             chart_gauge_dailygoal_reg.maxValue = dailyGoalRegMaxValue;
 
