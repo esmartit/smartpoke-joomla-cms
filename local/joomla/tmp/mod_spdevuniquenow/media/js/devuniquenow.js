1363e3dedadc619e3692058bb39068c0d914650c
adolfozignago <adolfo.zignago@esmartit.es>
2020-07-30 13:05:55 +0200

    Update modules: activitydate devuniquenow

--- local/joomla/tmp/mod_spdevuniquenow/media/js/devuniquenow.js
+++ local/joomla/tmp/mod_spdevuniquenow/media/js/devuniquenow.js
@@ -1,21 +1,12 @@
 $(document).ready( function() {
     let userTimeZone = document.getElementById('userTimeZone').innerText;
-    let inAct = 0;
-    let limitAct = 0;
-    let outAct = 0;
 
-    // let seUniqueNow = new EventSource("/index.php?option=com_spserverevent&format=json&base_url=ms_data&resource_path=/sensor-activity/now-detected-count?timezone="+userTimeZone);
-    let seUniqueNow = new EventSource("/index.php?option=com_spserverevent&format=json&base_url=ms_data&resource_path=/sensor-activity/now-detected?timezone="+userTimeZone);
+    let seUniqueNow = new EventSource("/index.php?option=com_spserverevent&format=json&base_url=ms_data&resource_path=/sensor-activity/v2/now-detected-count?timezone="+userTimeZone);
     let deviceNow = 0;
 
     seUniqueNow.onmessage = function (event) {
         let eventData = JSON.parse(event.data);
-        inAct = eventData.inCount;
-        limitAct = eventData.limitCount;
-        outAct = eventData.outCount;
-        deviceNow = inAct + limitAct + outAct;
-
-        // deviceNow = eventData.count;
+        deviceNow = eventData.count;
         document.getElementById("devuniquenow").innerHTML = Intl.NumberFormat().format(deviceNow);
     }
 });
