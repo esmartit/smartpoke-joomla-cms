b9984bf838a0222d596e63603ec40e674eac2098
adolfozignago <adolfo.zignago@esmartit.es>
2020-08-09 23:10:05 +0200

    helpers

---
+++ local/joomla/tmp/mod_spavgtimebigdata/helper.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package     SmartPoke.Site
+ * @subpackage  mod_spavgtimebigdata
+ *
+ * @copyright   Copyright (C) 2020 eSmartIT. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+class ModSPAvgTimeBigDataHelper
+{
+    /**
+     * Returns the userTime zone if the user has set one, or the global config one
+     * @return mixed
+     */
+    public static function getTimeZone() {
+        $userTz = JFactory::getUser()->getParam('timezone');
+        $timeZone = JFactory::getConfig()->get('offset');
+        if($userTz) {
+            $timeZone = $userTz;
+        }
+        return $timeZone;
+    }
+
+    function time2strAjax() {
+
+        $str = "";				// initialize variable
+        $time = $_REQUEST['data'];
+        $time = floor($time);
+
+        if (!$time)
+            return "0 s";
+
+        $d = $time/86400;
+        $d = floor($d);
+
+        if ($d) {
+            $str .= "$d d, ";
+            $time = $time % 86400;
+        }
+
+        $h = $time/3600;
+        $h = floor($h);
+        if ($h) {
+            $str .= "$h h, ";
+            $time = $time % 3600;
+        }
+
+        $m = $time/60;
+        $m = floor($m);
+
+        if ($m) {
+            $str .= "$m m, ";
+            $time = $time % 60;
+        }
+
+        if ($time)
+            $str .= "$time s, ";
+
+        $str = preg_replace("/, $/",'',$str);
+        return $str;
+    }
+
+}
