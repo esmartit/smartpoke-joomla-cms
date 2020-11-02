f599e3539a2354a2f1a057a77d508d63a199a6af
adolfozignago <adolfo.zignago@esmartit.es>
2020-11-02 16:51:29 +0100

    add detailed by time report. Also fix on detailed report clear a table.

--- local/joomla/tmp/mod_spreportbigdatadetailbytime/media/js/spreportbigdatadetailbytime.js
+++ local/joomla/tmp/mod_spreportbigdatadetailbytime/media/js/spreportbigdatadetailbytime.js
@@ -272,48 +272,27 @@ function getDeviceExList() {
         });
 }
 
-$(document).ready(function () {
-    let type = '';
-    $('#radio15m').on('change', function () {
-        type = $('#radio15m').val();
-        $('input[name="radioGroup"]:radio:checked').val(type);
-        showGroup($('#radio15m').val())
-    });
-
-    $('#radio30m').on('change', function () {
-        type = $('#radio30m').val();
-        $('input[name="radioGroup"]:radio:checked').val(type);
-        showGroup($('#radio30m').val())
-    });
-
-    $('#radio60m').on('change', function () {
-        type = $('#radio60m').val();
-        $('input[name="radioGroup"]:radio:checked').val(type);
-        showGroup($('#radio60m').val())
-    });
-
-});
-
-function showGroup(type) {
-    let groupType = type;
-    let request = {
-        option       : 'com_ajax',
-        module       : 'spreportbigdatadetailbytime',  // to target: mod_spreportbigdatadetailbytime
-        method       : 'showGroupBigData',  // to target: function showGroupBigDataAjax in class ModSPReportBigDataDetailByTimeHelper
-        format       : 'json',
-        data         : groupType
-    };
-    $.ajax({
-        method: 'POST',
-        data: request
-    })
-        .success(function(response) {
-            console.log('call success '+groupType);
-        })
-        .error(function() {
-            console.log('ajax call failed');
-        });
-}
+// $(document).ready(function () {
+//     let type = '';
+//     $('#radio15m').on('change', function () {
+//         type = $('#radio15m').val();
+//         $('input[name="radioGroup"]:radio:checked').val(type);
+//         showGroup($('#radio15m').val())
+//     });
+//
+//     $('#radio30m').on('change', function () {
+//         type = $('#radio30m').val();
+//         $('input[name="radioGroup"]:radio:checked').val(type);
+//         showGroup($('#radio30m').val())
+//     });
+//
+//     $('#radio60m').on('change', function () {
+//         type = $('#radio60m').val();
+//         $('input[name="radioGroup"]:radio:checked').val(type);
+//         showGroup($('#radio60m').val())
+//     });
+//
+// });
 
 $(document).ready(function() {
 
@@ -402,11 +381,21 @@ function sendForm() {
     let t_state = $('#selStateS').val();
     let t_city = $('#selCityS').val();
     let t_zipcode = $('#selZipCodeS').val();
+
     let t_spot = $('#selSpot').val();
     let t_sensor = $('#selSensor').val();
     let t_zone = $('#selZone').val();
-    let t_group = $('input[name="radioGroup"]:radio:checked').val();
     let t_type = $('#selType').val();
+
+    let t_brands = '';
+    let t_status = '';
+    let t_presence = '';
+    let t_ageS = '';
+    let t_ageE = '';
+    let t_sex = '';
+    let t_zipcodes = '';
+    let t_member = '';
+
     let userTimeZone = document.getElementById('userTimeZone').innerText;
 
     switch (t_type) {
@@ -422,13 +411,85 @@ function sendForm() {
             break;
     }
 
-    let dataForm = {
-        "dateStart": t_dateS, "dateEnd": t_dateE, "startTime": t_timeS, "endTime": t_timeE,
-        "countryId": t_country, "stateId": t_state, "cityId": t_city, "zipcodeId": t_zipcode,
-        "spotId": t_spot, "sensorId": t_sensor, "zoneId": t_zone, "includedDevices": t_devicesIn, "excludedDevices": t_devicesEx,
-        "group": t_group,
-        "timeZone": userTimeZone
-    }
+    evtSourceDetailbyTimeBigData
+    (
+        t_dateS, t_dateE, t_timeS, t_timeE,
+        t_country, t_state, t_city, t_zipcode,
+        t_spot, t_sensor, t_zone, t_devicesIn, t_devicesEx,
+        t_brands, t_status, t_presence,
+        t_ageS, t_ageE, t_sex, t_zipcodes, t_member,
+        userTimeZone
+    );
+}
 
-    console.log(dataForm);
+function evtSourceDetailbyTimeBigData(dateS, dateE, timeS, timeE, country, state, city, zipcode, spot, sensor, zone, inDevices, exDevices, brands, status, presence, ageS, ageE, sex,
+                                zipcodes, member, userTZ) {
+
+    let seActivityBigData = new EventSource("/index.php?option=com_spserverevent&format=json&base_url=ms_data&resource_path=/reports/listbytime?"+
+        "timezone="+userTZ+"%26startDate="+dateS+"%26endDate="+dateE+"%26startTime="+timeS+"%26endTime="+timeE+
+        "%26countryId="+country+"%26stateId="+state+"%26cityId="+city+"%26zipcodeId="+zipcode+
+        "%26spotId="+spot+"%26sensorId="+sensor+"%26zoneId="+zone+"%26includedDevices="+inDevices+"%26excludedDevices="+exDevices+
+        "%26brands="+brands+"%26status="+status+"%26presence="+presence+
+        "%26ageStart="+ageS+"%26ageEnd="+ageE+"%26gender="+sex+"%26zipCode="+zipcodes+"%26memberShip="+member+"%26groupBy=BY_HOUR");
+
+    tableDetail = $('#datatable-buttons').DataTable({
+        "destroy": true,
+        "column": [
+            {"data": "spotId"},
+            {"data": "sensorId"},
+            {"data": "groupDate"},
+            {"data": "total"}
+        ],
+        "dom": 'Bfrtip',
+        "buttons": [
+            {
+                "extend": 'copy',
+                "className": 'btn-sm'
+            },
+            {
+                "extend": 'csv',
+                "className": 'btn-sm'
+            },
+            {
+                "extend": 'excel',
+                "className": 'btn-sm'
+            },
+            {
+                "extend": 'pdfHtml5',
+                "className": 'btn-sm'
+            },
+            {
+                "extend": 'print',
+                "className": 'btn-sm'
+            },
+        ],
+        "responsive": true
+    });
+
+    NProgress.start();
+    NProgress.set(0,4);
+    tableDetail.clear();
+
+    seActivityBigData.onmessage = function (event) {
+        let eventData = JSON.parse(event.data);
+        let len = eventData.length;
+        for (let x=0; x<len; x++) {
+            let last = eventData[x].isLast;
+            if (eventData[x].body != null) {
+                let bodyData = eventData[x].body;
+                tableDetail.row.add(
+                    [
+                        bodyData.spotId,
+                        bodyData.sensorId,
+                        bodyData.groupDate,
+                        bodyData.total
+                    ]).draw(false);
+            } else {
+                if (last) {
+                    seActivityBigData.close();
+                    NProgress.done();
+                }
+            }
+        }
+    }
 }
