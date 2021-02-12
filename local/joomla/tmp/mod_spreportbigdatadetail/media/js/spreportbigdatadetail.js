3c76376e7bc491f1a7d66bcc444c5a9ddcc33b47
AZignago <adolfo.zignago@esmartit.es>
2021-02-12 16:21:57 +0100

    fix: Add HASH function to bigdata detail. Legal issues.

--- local/joomla/tmp/mod_spreportbigdatadetail/media/js/spreportbigdatadetail.js
+++ local/joomla/tmp/mod_spreportbigdatadetail/media/js/spreportbigdatadetail.js
@@ -404,6 +404,22 @@ function sendForm() {
     );
 }
 
+function hashFnv32a(str, asString, seed) {
+    /*jshint bitwise:false */
+    var i, l,
+        hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+    for (i = 0, l = str.length; i < l; i++) {
+        hval ^= str.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    if( asString ){
+        // Convert to 8 digit hex string
+        return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+    }
+    return hval >>> 0;
+}
+
 function evtSourceDetailBigData(dateS, dateE, timeS, timeE, country, state, city, zipcode, spot, sensor, zone, inDevices, exDevices, brands, status, presence, ageS, ageE, sex,
                                 zipcodes, member, userTZ) {
 
@@ -469,7 +485,7 @@ function evtSourceDetailBigData(dateS, dateE, timeS, timeE, country, state, city
                     [
                         bodyData.spotId,
                         bodyData.sensorId,
-                        bodyData.clientMac,
+                        bodyData.clientMac.substr(-8)+'-'+hashFnv32a(bodyData.clientMac, true, 'eSmartIT'),
                         bodyData.groupDate,
                         bodyData.rssi,
                         bodyData.status,
